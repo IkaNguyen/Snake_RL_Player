c50d00e329323e7a5f65164a08fb85e07ab2a7c4
IkaNguyen <nguyenquocthai51124@gmail.com>
2025-07-09 18:20:47 +0700

    update game

---
+++ model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+class Linear_QNetWork(nn.Module):
+    def _init_(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+    def forward(self, x):
+        x = F.relu(self.linear1)
